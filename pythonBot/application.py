5f7787bc2072f390f054471c5e3c9d0e5347f814
Marr10tt <Mackenziejmarriott@gmail.com>
2021-09-23 21:31:10 +0100

    moved music commands to seperate file

--- pythonBot/application.py
+++ pythonBot/application.py
@@ -4,6 +4,9 @@ import youtube_dl
 import os
 import random
 from dotenv import load_dotenv
+import musicCommands
+from musicCommands import *
+
 #Version 0.1.2
 
 #controls the bots prefix
@@ -27,69 +30,33 @@ leave - makes the bot leave the vc```""")
 
 
 #music commands
-
-#downloads and plays the song based on YT link
 @client.command()
 async def play(ctx, url : str):
-    song_there = os.path.isfile("song.webm")
-    try:
-        if song_there:
-            os.remove("song.webm")
-    #throws a user error message if 2 songs are tried to be played at once
-    except PermissionError:
-        await ctx.send("Wait for the current playing music to end or use the 'stop' command")
-        return
+    global client
+    await musicCommands.play(ctx, url, client)
 
-    #connects the bot to the general vc
-    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='general')
-    await voiceChannel.connect()
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    #249/250/251 sets it to download a webm. 
-    #discord.FFMpegOpusAudio allows it to play immediately after downloading without any conversion
-    ydl_opts = {
-        'format': '249/250/251',
-    }
-    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-        ydl.download([url])
-    for file in os.listdir("./"):
-        if file.endswith(".webm"):
-            os.rename(file, "song.webm")
-    voice.play(discord.FFmpegOpusAudio("song.webm"))
-
-#makes the bot leave the vc it is in
 @client.command()
 async def leave(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_connected():
-        await voice.disconnect()
-    else:
-        await ctx.send("The bot is not connected to a voice channel.")
+    global client
+    await musicCommands.leave(ctx, client)
 
 #pauses the current track 
 @client.command()
 async def pause(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_playing():
-        voice.pause()
-    else:
-        await ctx.send("Currently no audio is playing.")
+    global client
+    await musicCommands.pause(ctx, client)
 
 #resumes the current track after being paused
 @client.command()
 async def resume(ctx):
-    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
-    if voice.is_paused():
-        voice.resume()
-    else:
-        #throws error message
-        await ctx.send("The audio is not paused.")
+    global client
+    await musicCommands.resume(ctx, client)
 
 #stops the music (unable to resume afterwards)
 @client.command()
 async def stop(ctx):
     voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
     voice.stop()
-
 
 #other commands
 
---
+++ pythonBot/musicCommands.py
@@ -0,0 +1,54 @@
+import discord
+import youtube_dl
+import os
+from discord.ext import commands
+
+#downloads and plays the song based on YT link
+async def play(ctx, url : str, client):
+    song_there = os.path.isfile("song.webm")
+    try:
+        if song_there:
+            os.remove("song.webm")
+    #throws a user error message if 2 songs are tried to be played at once
+    except PermissionError:
+        await ctx.send("Wait for the current playing music to end or use the 'stop' command")
+        return
+
+    #connects the bot to the general vc
+    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name='general')
+    await voiceChannel.connect()
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    #249/250/251 sets it to download a webm. 
+    #discord.FFMpegOpusAudio allows it to play immediately after downloading without any conversion
+    ydl_opts = {
+        'format': '249/250/251',
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".webm"):
+            os.rename(file, "song.webm")
+    voice.play(discord.FFmpegOpusAudio("song.webm"))
+
+#makes the bot leave the vc it is in
+async def leave(ctx, client):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_connected():
+        await voice.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+async def resume(ctx, client):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_paused():
+        voice.resume()
+    else:
+        #throws error message
+        await ctx.send("The audio is not paused.")
+
+async def pause(ctx, client):
+    voice = discord.utils.get(client.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send("Currently no audio is playing.")
